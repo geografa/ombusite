f974b35affcc2f1e4e212e79f867c8412b0279d2
Martin Rio <martin@ombuweb.com>
2012-09-09 10:37:54 -0700

    adds fabfile

---
+++ fabfile.py
@@ -0,0 +1,33 @@
+from fabric.api import task, env
+#from butter import drush, deploy, drupal
+
+
+# Host settings
+@task
+def qa():
+    """
+    The qa server definition
+    """
+    env.hosts = ['ombu@d2.ombuweb.com:34165']
+    env.host_type = 'qa'
+    env.user = 'ombu'
+    env.host_webserver_user = 'www-data'
+    env.host_site_path = '/mnt/main/qa/qa2'
+
+
+@task
+def push(ref='origin/master'):
+    """
+    Deploy a commit to a host
+    """
+    from fabric.api import local, run, cd
+    from fabric.contrib.project import rsync_project
+    local('pelican -s config.py -d')
+    with cd(env.host_site_path):
+        run('rm -rf public && mkdir public')
+    rsync_project(
+        remote_dir=env.host_site_path + '/public',
+        local_dir='output/'
+    )
+    with cd(env.host_site_path):
+        run('chown -R ombu:www-data public && chmod -R 02750 public')
